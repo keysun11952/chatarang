57643b75fff769efa0a77ff551e1bedb8581ffb0
Junheng Wang <wangj14@rose-hulman.edu>
2018-06-12 14:46:21 -0400

    add Message.js and MessageList.js

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import ChatHeader from './ChatHeader.js'
+import MessageList from './MessageList.js'
 
 class Chat extends Component {
     render() {
         return (
             <div className="Chat">
                 <ChatHeader />
+                <MessageList />
             </div>
         )
     }
